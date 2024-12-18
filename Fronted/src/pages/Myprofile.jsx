65d78e71fde9c2879b785313490eacb87178053b
AryanV311 <159629630+AryanV311@users.noreply.github.com>
2024-12-18 23:55:55 +0530

    profile update

--- Fronted/src/pages/Myprofile.jsx
+++ Fronted/src/pages/Myprofile.jsx
@@ -1,17 +1,29 @@
 import { useContext, useState } from "react"
 import { AppContext } from "../context/AppContext";
-
+import { assets } from "../assets/assets";
 
 export const Myprofile = () => {
 
  const { usersData, setUsersData} = useContext(AppContext)
  console.log(usersData);
-
+  
   const [isEdit, setIsEdit] = useState(false);
+  const [image, setImage] = useState(false)
 
   return usersData && (
     <div className="max-w-lg flex flex-col gap-2 text-sm">
-      <img className="w-36 rounded" src={usersData.image} alt="" />
+
+      {
+        isEdit ? <label htmlFor="image">
+          <div className="inline-block relative cursor-pointer">
+            <img className="w-36 rounded opacity-75" src={image ? URL.createObjectURL(image):usersData.image} alt="" />
+            <img className="absolute w-10 right-12 bottom-12" src={image ? '':assets.upload_icon} alt="" />
+          </div>
+          <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden />
+        </label>
+        : <img className="w-36 rounded" src={usersData.image} alt="" />
+      }
+
       {
         isEdit ? <input className="bg-gray-50 text-3xl font-medium max-w-60 mt-4" type="text" value={usersData.name} onChange={(e) => setUsersData(prev=> ({...prev, name:e.target.value}))} />
         : <p className="font-medium text-3xl text-neutral-800 mt-4">
